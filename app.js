dfced12788683a40832d98fcdc4c622f04dc114f
Isaiah Harris <harrisfambam@gmail.com>
2019-01-16 13:27:33 -1000

    start readme directions

---
+++ app.js
@@ -0,0 +1,7 @@
+// jQuery v3.3.1 is supported
+$('.menu-icon').click(function() {
+  $('.panel').toggleClass('show-menu');
+  $('.menu').toggleClass('active');
+  $('.circle-1').toggleClass('slide');
+  $('.dash-top').toggleClass('slide-top');
+});
